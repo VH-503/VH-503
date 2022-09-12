8105bd1cfd788635431c69dfe0335691cf54a8fe
vayaherman@gmail.com <vayaherman@gmail.com>
2022-09-12 19:17:26 +0300

    First commit Run the spring project

---
+++ src/main/java/com/example/eGlob/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.eGlob;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RequestMapping("api/hello-world")
+@RestController
+public class HelloWorldController {
+
+    //GET PUT POST DELETE
+    @GetMapping
+    public List<String> getHelloWorld(){return List.of("Hello", "World!");}
+}
